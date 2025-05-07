3c7fd433a0c65aa21727b3ff1e7b1dc0b3b16bca
StenHendrikLahe <sten-hendrik.lahe@tktk.ee>
2025-05-07 18:31:41 +0300

    Add files via upload

---
+++ UserGenderValidation/Program (1).cs
@@ -0,0 +1,19 @@
+﻿//küsib kasutaja soo m/n
+//küsib kasutaja perekonnanime
+//lähtudes valikutest, tervitab järgmiselt
+//"Tere, Hr. [perekonnanimi]" / "Tere, Pr. [perekonnanimi]"
+
+char sugu;
+string perekonnanimi;
+
+Console.WriteLine("Palun valige oma sugu (m/n):");
+sugu = char.Parse(Console.ReadLine());
+
+Console.WriteLine("Palun sisestage oma perekonnanimi:");
+perekonnanimi = Console.ReadLine();
+
+if (sugu == 'm')
+    { Console.WriteLine($"Tere Hr. {perekonnanimi}"); }
+else  if (sugu == 'n')
+    { Console.WriteLine($"Tere Pr. {perekonnanimi}"); }
+else { Console.WriteLine($"Tere, {perekonnanimi}"); }
