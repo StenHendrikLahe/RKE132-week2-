edf10f4f531f638211149280aedb6b1f2d8af4b3
StenHendrikLahe <sten-hendrik.lahe@tktk.ee>
2025-05-07 18:30:24 +0300

    Add files via upload

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,23 @@
+﻿//küsib kasutaja vanust
+//kui vanus < 13, kuva
+//"Te olete Instagram-i jaoks liiga noor"
+//muul juhul
+//kuva "Tere tulemast Instagram-i"
+
+string vanus;
+int vanusNR = 0;
+
+
+Console.WriteLine("Sisestage oma vanus:");
+
+vanus = Console.ReadLine();
+
+bool onVanusnr = int.TryParse(vanus, out vanusNR); //kas input on number [true/false]
+
+if (onVanusnr)
+{
+    if (vanusNR >= 13) //kas kasutaja on vanem kui 13(k.a)
+    { Console.WriteLine("Tere tulemast Instagram-i!"); }
+    else { Console.WriteLine("Te olete Instagram-i jaoks liiga noor!"); } //kasutaja on noorem kui 13
+}
+else { Console.WriteLine("Sisestage korrektne vanus!"); } //kui inputis oli midagi peale numbrite
